80a8a69c586d87c93a732570c2ac8383e342abce
leoneljuarez575-a11y <leoneljuarez575@gmail.com>
2025-08-24 17:27:09 -0300

    conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv("mysql.railway.internal");
-$user = getenv("root");
-$pass = getenv("pvxnlarxDSXefidLYIxYxACwkcBfucNk");
-$name = getenv("railway");
-$port = getenv("3306") ?: 3306;
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
+$port = getenv("DB_PORT") ?: 3306;
 
 $conexion = mysqli_connect($host, $user, $pass, $name, (int)$port);
 
