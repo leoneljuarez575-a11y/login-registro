1312b7098714bd06686923ea3779f25623019d1b
leoneljuarez575-a11y <leoneljuarez575@gmail.com>
2025-08-24 17:06:57 -0300

    conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,12 +1,13 @@
 <?php
-$conexion = mysqli_connect("localhost", "usuario", "", "login_register_db");
+$host = "${{RAILWAY_PRIVATE_DOMAIN}}";  // el que te da Railway
+$usuario = "root";                            // el que te da Railway
+$clave = "pvxnlarxDSXefidLYIxYxACwkcBfucNk";                   // la contraseña de Railway
+$base_datos = "${{MYSQL_DATABASE}}";                      // el nombre de la DB
+$puerto = 3306;                               // el puerto que te asignaron
 
-if($conexion){
-    echo 'Conectada correctamente a la DB';
+$conexion = mysqli_connect($host, $usuario, $clave, $base_datos, $puerto);
 
-}else {
-    echo 'No se pudo conectar a la DB';
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
-
-
 ?>
